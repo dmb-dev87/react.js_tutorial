dc140fd8f206cac495f7537c24c597d4709116de
dmb-dev87 <dmbdev800@gmail.com>
2020-09-24 10:19:16 -0600

    [TopOneExpert][2020-09-24][add vue tutorial]

--- vue_tutorial/vue-app/src/App.vue
+++ vue_tutorial/vue-app/src/App.vue
@@ -2,18 +2,49 @@
   <div id="app" class="small-container">
     <h1>Employees</h1>
 
-    <employee-table v-bind:employees="employees" />
-    
+    <employee-form @add:employee="addEmployee" />
+    <employee-table 
+      :employees="employees" 
+      @delete:employee="deleteEmployee" 
+      @edit:employee="editEmployee"
+    />
   </div>
 </template>
 
 <script>
   import EmployeeTable from '@/components/EmployeeTable.vue'
+  import EmployeeForm from '@/components/EmployeeForm.vue'
 
   export default {
     name: 'app',
     components: {
       EmployeeTable,
+      EmployeeForm,
+    },
+
+    methods: {
+      addEmployee(employee) {
+        const lastId = 
+          this.employees.length > 0
+            ? this.employees[this.employees.length - 1].id
+            : 0;
+        const id = lastId + 1;
+        const newEmployee = { ...employee, id };
+
+        this.employees = [...this.employees, newEmployee]
+      },
+
+      deleteEmployee(id) {
+        this.employees = this.employees.filter(
+          employee => employee.id !== id
+        )
+      },
+
+      editEmployee(id, updatedEmployee) {
+        this.employee = this.employees.map(employee =>
+          employee.id === id ? updatedEmployee : employee
+        )
+      },
     },
 
     data() {
@@ -36,7 +67,7 @@
           },
         ],
       }
-    }
+    }    
   }
 </script>
 
